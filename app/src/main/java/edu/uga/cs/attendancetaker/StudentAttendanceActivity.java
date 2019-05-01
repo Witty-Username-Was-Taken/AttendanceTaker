63ece9766c68238d3b720fd9780f34d0dd4de3e2
Sazz <srijal.joshy@gmail.com>
2019-05-01 12:30:24 -0400

    Done with recyclerview for Attendance

--- app/src/main/java/edu/uga/cs/attendancetaker/StudentAttendanceActivity.java
+++ app/src/main/java/edu/uga/cs/attendancetaker/StudentAttendanceActivity.java
@@ -1,44 +1,46 @@
 package edu.uga.cs.attendancetaker;
 
 import android.content.Context;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CourseOptionsActivity extends AppCompatActivity {
+public class StudentAttendanceActivity extends AppCompatActivity {
 
-    public static final int COURSE_OPTIONS_ACTIVITY_ID = 1;
 
-    private static final String TAG = "CourseOptionsActivity";
+    public static final int STUDENT_ATTENDANCE_ACTIVITY_ID = 2;
+
+    private static final String TAG = "StudentAttendance";
+    public static final String KEY_STATUS = "status";
+
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mRecyclerAdapter;
 
+
     List<String> genericDataList;// since we are reusing the same recyclerview
+    List<String> datesList = new ArrayList<>();
     Context context;
+    String crnFromPreviousActivity;
+
 
     // firebase stuff
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -48,32 +50,17 @@ public class CourseOptionsActivity extends AppCompatActivity {
     public static final String KEY_CLASS_NAME = "className";
 
 
-    List<String> classList = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course_options);
+        setContentView(R.layout.activity_student_attendance);
 
-        // 1. get data from the database and populate an Arraylist
-        // 2. populate the arraylist data into the reyclerview
-        context = getApplicationContext();
+        Intent intent = getIntent();
+        crnFromPreviousActivity = intent.getStringExtra("CRN_STRING");
+        Log.d(TAG, "onCreate: " + crnFromPreviousActivity);
 
         getStudentsClasses();
-
-//        loadRecycleriew();
     }
-
-    private void loadRecycleriew() {
-        mRecyclerView = findViewById(R.id.recyclerView);
-
-        mRecyclerAdapter = new GenericRecyclerAdapter(getApplicationContext(), genericDataList, COURSE_OPTIONS_ACTIVITY_ID);
-        mRecyclerView.setAdapter(mRecyclerAdapter);
-
-        mLayoutManager = new LinearLayoutManager(context);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-    }
-
 
 
     private void getStudentsClasses() {
@@ -83,20 +70,37 @@ public class CourseOptionsActivity extends AppCompatActivity {
         Log.d(TAG, "getStudentsClasses: Current user is " + currentUser.getUid());
 
         attendanceRef.whereEqualTo("student", currentUser.getUid())
+                .whereEqualTo("crn", crnFromPreviousActivity)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
+                            Map<String, Object> dataMap;
                             for (QueryDocumentSnapshot document : task.getResult()) {
 
-                                String crn = document.getString(KEY_CRN);
-                                String className = document.getString(KEY_CLASS_NAME);
-                                classList.add(crn + " | " + className);
+                                // Add both, the date and present/absent status
+
+                                dataMap = document.getData();
+
+                                Log.e(TAG, "onComplete: datamap " + dataMap.get(KEY_STATUS), null);
+
+
+                                HashMap<String, String> dateStatusMap = (HashMap<String, String>) dataMap.get(KEY_STATUS); // get the map of {date: Present/Absent}
+                                Log.e(TAG, "onComplete: datestatusmap " + dateStatusMap, null);
+
+                                for (String dateKey : dateStatusMap.keySet()) {
+                                    // Add data to make sure that the dates displayed don't exceed the current timestamp
+                                    // if date < currentTimestamp then add to list
+                                    String formattedStatus = dateKey + "\t" + dateStatusMap.get(dateKey);
+                                    datesList.add(formattedStatus);
+
+
+                                }
 
                             }
 
-                            genericDataList = classList;
+                            genericDataList = datesList;
                             loadRecycleriew();
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
@@ -106,4 +110,13 @@ public class CourseOptionsActivity extends AppCompatActivity {
 
     }
 
+    private void loadRecycleriew() {
+        mRecyclerView = findViewById(R.id.recyclerViewAttendance);
+
+        mRecyclerAdapter = new GenericRecyclerAdapter(getApplicationContext(), genericDataList, STUDENT_ATTENDANCE_ACTIVITY_ID); // -1 means don't use this activity's recycler view element's onclick
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+
+        mLayoutManager = new LinearLayoutManager(context);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+    }
 }
